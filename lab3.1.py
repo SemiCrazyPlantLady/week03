c6970a4944ba30fe0299408ececdad5d3d72e068
Deborah Anne Gormley <debi.gormley@gmail.com>
2023-02-16 18:56:06 +0000

    first lab of the week - messing around

---
+++ lab3.1.py
@@ -0,0 +1,9 @@
+# lab3.1
+#this program p is messing aound with types
+#Author Debi Gormley
+
+answer = 12
+print (f'The answer is: {answer}')
+print (f'The answer is' +str (answer))
+print (f'The type of answer is + {type (answer)}')
+
