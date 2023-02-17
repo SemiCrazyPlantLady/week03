eee45d24c7bf011913ad2f353e70f5a0ff75a162
Deborah Anne Gormley <debi.gormley@gmail.com>
2023-02-17 17:19:04 +0000

    subtracts on decimal# from the other

---
+++ sub2float.py
@@ -0,0 +1,8 @@
+# testTypes.py
+# this program subtracts one number from another, including decimals
+# Author Debi Gormley
+
+x = float(input ("Enter first number (include decimal if applicable): "))
+y = float(input('Enter second number (include decimal if applicable): '))
+answer = x - y
+print ("{} minus {} is {}" .format (x, y, answer))
