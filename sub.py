c1db7a7f52e9713fd2daa73ef2b12db8493d68e7
Deborah Anne Gormley <debi.gormley@gmail.com>
2023-02-17 17:11:41 +0000

    wwhole number minus whole number = answer

---
+++ sub.py
@@ -0,0 +1,8 @@
+# testTypes.py
+# this program subtracts one number from another
+# Author Debi Gormley
+
+x = int(input ("Enter first number: "))
+y = int(input('Enter second number: '))
+answer = x - y
+print ("{} minus {} is {}" .format (x, y, answer))
